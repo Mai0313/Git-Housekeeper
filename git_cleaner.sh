8477ca3d14d2b4524054693189f1cd5357487e33
Wei <21039366+Mai0313@users.noreply.github.com>
2023-06-01 09:00:56 +0000

    main scripts

---
+++ git_cleaner.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Download BFG.jav
+wget https://repo1.maven.org/maven2/com/madgag/bfg/1.13.0/bfg-1.13.0.jar
+
+# Rename
+mv bfg-1.13.0.jar bfg.jar
+
+# Scan all files that are over 10MB and remove it
+java -jar bfg.jar --strip-blobs-bigger-than 10M .git
+
+# Clean git
+git reflog expire --expire=now --all && git gc --prune=now --aggressive
+
+# Push to remote
+git push
+
+
+chmod +x ./git_diff.sh
+./git_diff.sh
